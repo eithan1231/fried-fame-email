d23508936992d24f9051fdafa553995b48155f1e
Eithan Hersey-Tuit <eithantuit10@gmail.com>
2021-03-24 22:59:47 +1100

    SMTP SSL Policy Selection Flexibility

--- library/router/sendmail.js
+++ library/router/sendmail.js
@@ -1,12 +1,40 @@
 const nodemailer = require('nodemailer');
 
+const getSecurityPolicies = () => {
+  switch (process.env.SMTP_SECURE) {
+    case 'STRICT':
+      return {
+        secure: true,
+        requireTLS: true
+      };
+
+    case 'STARTTLS':
+      return {
+        secure: false,
+        requireTLS: true
+      };
+
+    case 'NONE':
+      return {
+        secure: false,
+        ignoreTLS: true
+      };
+
+    default:
+      // return nodemailer defaults
+      return { };
+  }
+}
+
 const sendmail = async(options) => {
   try {
+    const securityPolicies = getSecurityPolicies();
+
     // creating mail transporter
     const transporter = nodemailer.createTransport({
+      ...securityPolicies,
       host: process.env.SMTP_HOSTNAME,
       port:  process.env.SMTP_PORT,
-      secure: process.env.SMTP_SECURE,
       auth: {
         user: process.env.SMTP_USERNAME,
         pass: process.env.SMTP_PASSWORD
